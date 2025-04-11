82bf0ab0e61c2c8e8677df448217d2e6698b0264
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-04-11 19:40:02 -0300

    Responsividade do header

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
 function clicar_menu(){
     const menu_navegacao = document.getElementById('menu_links');
     menu_navegacao.classList.toggle('show');  // troca entre adicionar e remover
+    const btn = document.getElementById('menu_navegacao');
+    if(btn.style.transform === "rotate(180deg)"){
+        btn.style.transform = "rotate(0deg)";
+    }else{
+        btn.style.transform = "rotate(180deg)";
+    }
+    /*
+        Checa se o botão já está rotacionado (rotate(180deg)).
+        Se sim, volta pra posição normal (rotate(0deg)).
+        Se não, aplica a rotação.
+    */
+    btn.style.transition = "0.5s"
 }
