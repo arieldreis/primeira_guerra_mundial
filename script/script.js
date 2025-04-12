7927600fd95a186287166aea4068e0b001e8fb00
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-04-12 19:38:11 -0300

    Formspree

--- script/script.js
+++ script/script.js
@@ -20,11 +20,38 @@ function voltar_cima(){
         behavior:"smooth" // Rolagem suave
     });
 }
-function verificacao(){
+function verificacao(event){
     event.preventDefault();
+    // variavel do form
+    const form = document.querySelector('.formDuvidas');
+    // Variaveis dos inputs
     const nomegrupo = document.getElementById('nome_grupo');
     const duvida = document.getElementById('duvida');
     // Verificação da etapa de dados.
-    const groupname = nomegrupo.value;
-    const doubt = duvida.value;
+    const groupname = nomegrupo.value.trim();
+    const doubt = duvida.value.trim();
+    const div = document.getElementById('content');
+
+    div.innerHTML = ""; // Limpa mensagens anteriores
+    nomegrupo.style.border = "";
+    duvida.style.border = "";
+
+    if(groupname === "" && doubt === ""){
+        div.innerHTML = "Preencha os dois campos de Nome do Grupo e a sua dúvida."
+        nomegrupo.style.border = "2px solid red";
+        duvida.style.border = "2px solid red";
+        div.style.color = "red";
+    }else if(groupname === ""){
+        div.innerHTML = "Preencha o campo de nome do grupo.";
+        nomegrupo.style.border = "2px solid red";
+        return;
+    }else if(doubt === ""){
+        div.innerHTML = "Preencha a sua dúvida, será importante para nosso trabalho."
+        duvida.style.border = "2px solid red";
+        return;
+    }
+    div.style.color = "red";
+    div.style.marginBottom = "15px"
+    div.style.textTransform = "uppercase";
+    form.submit();
 }
